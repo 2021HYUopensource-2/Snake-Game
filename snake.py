a5f129ac54a9f1244eb9e6fb98043b2653114eac
Yena <yena1885@naver.com>
2021-11-21 19:05:13 +0900

    Add message box

--- snake.py
+++ snake.py
@@ -162,35 +162,48 @@ def randomSnack(rows, item):
     return (x,y)
 
 
-def main():
-    global s, snack, win
-    win = pygame.display.set_mode((width,height))
-    s = snake((255,0,0), (10,10))
-    s.addCube()
-    snack = cube(randomSnack(rows,s), color=(0,255,0))
-    flag = True
-    clock = pygame.time.Clock()
-    
-    while flag:
-        pygame.time.delay(50)
-        clock.tick(10)
-        s.move()
-        headPos = s.head.pos
-        if headPos[0] >= 20 or headPos[0] < 0 or headPos[1] >= 20 or headPos[1] < 0:
-            print("Score:", len(s.body))
-            s.reset((10, 10))
 
-        if s.body[0].pos == snack.pos:
-            s.addCube()
-            snack = cube(randomSnack(rows,s), color=(0,255,0))
+def message_box(subject, content):
+	root = tk.Tk()
+	root.attributes("-topmost", True)
+	root.withdraw()
+	messagebox.showinfo(subject, content)
+	try:
+		root.destroy()
+	except:
+		pass
+
+
+def main():
+	global s, snack, win
+	win = pygame.display.set_mode((width,height))
+	s = snake((255,0,0), (10,10))
+	s.addCube()
+	snack = cube(randomSnack(rows,s), color=(0,255,0))
+	flag = True
+	clock = pygame.time.Clock()
+    
+	while flag:
+		pygame.time.delay(50)
+		clock.tick(10)
+		s.move()
+		headPos = s.head.pos
+		if headPos[0] >= 20 or headPos[0] < 0 or headPos[1] >= 20 or headPos[1] < 0:
+			print("Score:", len(s.body))
+			s.reset((10, 10))
+
+		if s.body[0].pos == snack.pos:
+			s.addCube()
+			snack = cube(randomSnack(rows,s), color=(0,255,0))
             
-        for x in range(len(s.body)):
-            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
-                print("Score:", len(s.body))
-                s.reset((10,10))
-                break
+		for x in range(len(s.body)):
+			if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+				print("Score:", len(s.body))
+				message_box('You Lost!', 'Play again')
+				s.reset((10,10))
+				break
                     
-        redrawWindow()
+		redrawWindow()
 
 main()
     
