44de8c37ef3b34cb10ff0b73eb051ba3ac1f4adf
Yena <yena1885@naver.com>
2021-12-13 19:51:51 +0900

    color

--- snake.py
+++ snake.py
@@ -26,6 +26,7 @@ height = (6 - difficulty) * 100 #500=1 400=2 300=3
 cols = (6 - difficulty) * 4 #20=1 16=2 12=3
 rows = (6 - difficulty) * 4 #20=1 16=2 12=3
 
+
 class cube():
     rows = 20
     w = 500
@@ -58,82 +59,83 @@ class cube():
 
 
 class snake():
-    body = []
-    turns = {}
+	body = []
+	turns = {}
     
-    def __init__(self, color, pos):
-        #pos is given as coordinates on the grid ex (1,5)
-        self.color = color
-        self.head = cube(pos)
-        self.body.append(self.head)
-        self.dirnx = 0
-        self.dirny = 1
+	def __init__(self, color, pos):
+		#pos is given as coordinates on the grid ex (1,5)
+		self.color = color
+		self.head = cube(pos)
+		self.body.append(self.head)
+		self.dirnx = 0
+		self.dirny = 1
     
-    def move(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            keys = pygame.key.get_pressed()
+	def move(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+			keys = pygame.key.get_pressed()
 
-            for key in keys:
-                if keys[pygame.K_LEFT]:
-                    self.dirnx = -1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
-                elif keys[pygame.K_RIGHT]:
-                    self.dirnx = 1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
-                elif keys[pygame.K_UP]:
-                    self.dirny = -1
-                    self.dirnx = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
-                elif keys[pygame.K_DOWN]:
-                    self.dirny = 1
-                    self.dirnx = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+			for key in keys:
+				if keys[pygame.K_LEFT]:
+					self.dirnx = -1
+					self.dirny = 0
+					self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+				elif keys[pygame.K_RIGHT]:
+					self.dirnx = 1
+					self.dirny = 0
+					self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+				elif keys[pygame.K_UP]:
+					self.dirny = -1
+					self.dirnx = 0
+					self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+				elif keys[pygame.K_DOWN]:
+					self.dirny = 1
+					self.dirnx = 0
+					self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
         
-        for i, c in enumerate(self.body):
-            p = c.pos[:]
-            if p in self.turns:
-                turn = self.turns[p]
-                c.move(turn[0], turn[1])
-                if i == len(self.body)-1:
-                    self.turns.pop(p)
-            else:
-                c.move(c.dirnx,c.dirny)
+		for i, c in enumerate(self.body):
+			p = c.pos[:]
+			if p in self.turns:
+				turn = self.turns[p]
+				c.move(turn[0], turn[1])
+				if i == len(self.body)-1:
+					self.turns.pop(p)
+			else:
+				c.move(c.dirnx,c.dirny)
         
         
-    def reset(self,pos):
-        self.head = cube(pos)
-        self.body = []
-        self.body.append(self.head)
-        self.turns = {}
-        self.dirnx = 0
-        self.dirny = 1
+	def reset(self,pos):
+		self.head = cube(pos)
+		self.body = []
+		self.body.append(self.head)
+		self.turns = {}
+		self.dirnx = 0
+		self.dirny = 1
 
-    def addCube(self):
-        tail = self.body[-1]
-        dx, dy = tail.dirnx, tail.dirny
+	def addCube(self):
+		tail = self.body[-1]
+		dx, dy = tail.dirnx, tail.dirny
 
-        if dx == 1 and dy == 0:
-            self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
-        elif dx == -1 and dy == 0:
-            self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
-        elif dx == 0 and dy == 1:
-            self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
-        elif dx == 0 and dy == -1:
-            self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
+		if dx == 1 and dy == 0:
+			self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
+		elif dx == -1 and dy == 0:
+			self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
+		elif dx == 0 and dy == 1:
+			self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
+		elif dx == 0 and dy == -1:
+			self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
 
-        self.body[-1].dirnx = dx
-        self.body[-1].dirny = dy
+		self.body[-1].dirnx = dx
+		self.body[-1].dirny = dy
     
-    def draw(self, surface):
-        for i,c in enumerate(self.body):
-            if i == 0:
-                c.draw(surface, True)
-            else:
-                c.draw(surface)
+
+	def draw(self, surface):
+		for i,c in enumerate(self.body):
+			if i == 0:
+				c.draw(surface, True)
+			else:
+				c.draw(surface)
 
 
 
@@ -187,37 +189,46 @@ def message_box(subject, content):
     except:
         pass
 
-
 def main():
-    global s, snack, win
-    win = pygame.display.set_mode((width,height))
-    s = snake((255,0,0), (10,10))
-    snack = cube(randomSnack(rows,s), color=(0,255,0))
-    flag = True
-    clock = pygame.time.Clock()
+	global s, snack, win
+	win = pygame.display.set_mode((width,height))
+	s = snake((255,0,0),(10,10))
+	if difficulty == 1:
+		snack = cube(randomSnack(rows,s), color=(0,255,0))
+	elif difficulty == 2:
+		snack = cube(randomSnack(rows,s), color=(255,0,255))
+	elif difficulty == 3:
+		snack = cube(randomSnack(rows,s), color=(255,128,0))
+	flag = True
+	clock = pygame.time.Clock()
     
-    while flag:
-        pygame.time.delay(50)
-        clock.tick(difficulty*5)
-        s.move()
-        headPos = s.head.pos
-        if headPos[0] >= fail_row_col or headPos[0] < 0 or headPos[1] >= fail_row_col or headPos[1] < 0:
-            print("Score:", len(s.body))
-            message_box('You Lost!', 'Play again')
-            s.reset((10, 10))
+	while flag:
+		pygame.time.delay(50)
+		clock.tick(difficulty*5)
+		s.move()
+		headPos = s.head.pos
+		if headPos[0] >= fail_row_col or headPos[0] < 0 or headPos[1] >= fail_row_col or headPos[1] < 0:
+			print("Score:", len(s.body))
+			message_box('You Lost!', 'Play again')
+			s.reset((10, 10))
 
-        if s.body[0].pos == snack.pos:
-            s.addCube()
-            snack = cube(randomSnack(rows,s), color=(0,255,0))
+		if s.body[0].pos == snack.pos:
+			s.addCube()
+			if difficulty == 1:
+				snack = cube(randomSnack(rows,s), color=(0,255,0))
+			elif difficulty == 2:
+				snack = cube(randomSnack(rows,s), color=(255,0,255))
+			elif difficulty == 3:
+				snack = cube(randomSnack(rows,s), color=(255,128,0))
             
-        for x in range(len(s.body)):
-            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
-                print("Score:", len(s.body))
-                message_box('You Lost!', 'Play again')
-                s.reset((10,10))
-                break
+		for x in range(len(s.body)):
+			if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+				print("Score:", len(s.body))
+				message_box('You Lost!', 'Play again')
+				s.reset((10,10))
+				break
                     
-        redrawWindow()
+		redrawWindow()
 
 main()
     
